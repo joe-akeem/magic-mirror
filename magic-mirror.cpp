a1b24934793176819d8603dcb05e420786cb80cd
Joachim Lengacher <j.lengacher@gmx.net>
2015-03-09 22:19:09 +0000

    training from camera. rectangle around detected face.

--- magic-mirror.cpp
+++ magic-mirror.cpp
@@ -22,6 +22,8 @@ using namespace std;
 using namespace cv;
 using namespace raspicam;
 
+const string WINDOW_NAME = "Magic Mirror";
+
 void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels,
 		char separator = ';') {
 	puts("Reading CSV file...");
@@ -44,77 +46,158 @@ void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels,
 	puts("Done reading CSV file.");
 }
 
-void train(const string& trainingDataCsvFile, Ptr<FaceRecognizer>& model) {
+void trainFromCamera(RaspiCam_Cv& camera, CascadeClassifier& face_cascade, Ptr<FaceRecognizer>& model) {
+	cout << "Training by capturing from camera" << endl;
+	int trainingImageCount = 0;
+	Mat captured;
+	vector<Rect> faces;
 	vector<Mat> trainingImages;
 	vector<int> trainingLabels;
-	puts("starting training...");
+	while (trainingImageCount < 10) {
+		try {
+			camera.grab();
+			camera.retrieve(captured);
+
+			face_cascade.detectMultiScale(captured, faces, 1.1, 2, 0,Size(80, 80));
+			if (faces.size() == 1) {
+				trainingImageCount++;
+				cout << "detected face for training" << endl;
+				for (int i = 0; i < faces.size(); i++) {
+
+					int x = faces[i].x;
+					int y = faces[i].y;
+					int h = y + faces[i].height;
+					int w = x + faces[i].width;
+					rectangle(captured, Point(x, y), Point(w, h),
+							Scalar(0, 255, 0), 2, 8, 0);
+
+					Mat cropedFace = captured(faces[i]);
+					captured.copyTo(cropedFace);
+
+					Size size(200, 200);
+					Mat resizedCropedFace;
+					resize(cropedFace, resizedCropedFace, size);
+
+					trainingImages.push_back(resizedCropedFace);
+					trainingLabels.push_back(0);
+				}
+			} else {
+				cout << "no face detected" << endl;
+			}
+			imshow(WINDOW_NAME, captured);
+			waitKey(30);
+		} catch (cv::Exception& e) {
+			cerr << "Error: " << e.msg << endl;
+		}
+	}
+	cout << "Starting training..." << endl;
+	model->train(trainingImages, trainingLabels);
+	cout << "Training completed" << endl;
+}
+
+void trainFromCsv(const string& trainingDataCsvFile, Ptr<FaceRecognizer>& model) {
+	vector<Mat> trainingImages;
+	vector<int> trainingLabels;
+	cout << "Training from CSV file '" << trainingDataCsvFile << "'" << endl;;
 	try {
 		read_csv(trainingDataCsvFile, trainingImages, trainingLabels);
 	} catch (cv::Exception& e) {
-		cerr << "Error opening file \"" << trainingDataCsvFile << ": " << e.msg
-				<< endl;
-		exit(1);
+		cerr << "Error opening file \"" << trainingDataCsvFile << ": " << e.msg << endl;
+		exit(EXIT_FAILURE);
 	}
-
+	cout << "Starting training..." << endl;
 	model->train(trainingImages, trainingLabels);
-	puts("training completed.");
+	cout << "Training completed" << endl;
+}
+
+string getNameForLabel(int label) {
+	switch(label) {
+		case 1:
+			return "Joe";
+		case 0:
+			return "Lasse";
+	}
+	return "Unbekannt";
 }
 
 int main(int argc, const char *argv[]) {
-	if (argc < 2) {
-		cout << "usage: " << argv[0] << " <csv.ext> <output_folder> " << endl;
-		exit(1);
-	}
-
-	string output_folder = ".";
-	if (argc == 3) {
-		output_folder = string(argv[2]);
-	}
-
-	string trainingDataCsvFile = string(argv[1]);
-	Ptr<FaceRecognizer> model = createEigenFaceRecognizer();
-	train(trainingDataCsvFile, model);
-
 	cout << "Opening Camera..." << endl;
 	RaspiCam_Cv camera;
 	camera.set(CV_CAP_PROP_FORMAT, CV_8UC1);
+
+	Ptr<FaceRecognizer> model = createEigenFaceRecognizer();
+	//Ptr<FaceRecognizer> model = createEigenFaceRecognizer(0, 200.0);
+
+
 	if (camera.open()) {
 		CascadeClassifier face_cascade;
 		if (face_cascade.load("/usr/local/share/OpenCV/lbpcascades/lbpcascade_frontalface.xml")) {
+			namedWindow(WINDOW_NAME, WINDOW_AUTOSIZE);
+
+			if (argc < 2) {
+				trainFromCamera(camera, face_cascade, model);
+			} else {
+				string trainingDataCsvFile = string(argv[1]);
+				trainFromCsv(trainingDataCsvFile, model);
+			}
+
 			std::vector<Rect> faces;
 			Mat captured;
+
 			while (1) {
-				camera.grab();
-				camera.retrieve(captured);
-				//imwrite("camptured.jpg", image);
+				try {
+					camera.grab();
+					camera.retrieve(captured);
 
-				face_cascade.detectMultiScale(captured, faces, 1.1, 2, 0, Size(80, 80));
-				if (faces.size() > 0) {
-					cout << "detected " << faces.size() << " faces" << endl;
-					for (int i = 0; i < faces.size(); i++) {
+					face_cascade.detectMultiScale(captured, faces, 1.1, 2, 0, Size(80, 80));
+					if (faces.size() > 0) {
+						cout << "detected " << faces.size() << " faces" << endl;
+						for (int i = 0; i < faces.size(); i++) {
 
-						Mat cropedFace = captured(faces[i]);
-						cropedFace.copyTo(captured);
+							int x = faces[i].x;
+							int y = faces[i].y;
+							int h = y + faces[i].height;
+							int w = x + faces[i].width;
+							rectangle(captured, Point(x, y), Point(w, h), Scalar(0, 255, 0), 2, 8, 0);
 
-						Size size(200,200);
-						Mat resizedCropedFace;
-						resize(cropedFace, resizedCropedFace, size);
 
-						int predictedLabel = model->predict(resizedCropedFace);
-						string result_message = format("Predicted label = %d.", predictedLabel);
-						cout << result_message << endl;
+							Mat cropedFace = captured(faces[i]);
+							//cropedFace.copyTo(captured);
+							captured.copyTo(cropedFace);
+
+							Size size(200,200);
+							Mat resizedCropedFace;
+							resize(cropedFace, resizedCropedFace, size);
+
+							int predictedLabel = -1;
+							double confidence = 0.0;
+
+							//predictedLabel = model->predict(resizedCropedFace);
+							model->predict(resizedCropedFace, predictedLabel, confidence);
+							string result_message = format("Predicted label = %d, Confidence = %f.", predictedLabel, confidence);
+							cout << result_message << endl;
+
+							putText(captured, getNameForLabel(predictedLabel), Point(x, y - 5),
+									FONT_HERSHEY_COMPLEX_SMALL, 0.8, Scalar(0, 255, 0), 1, CV_AA);
+						}
+					} else {
+						cout << "no face detected" << endl;
 					}
-				} else {
-					cout << "no face detected" << endl;
+					imshow(WINDOW_NAME, captured);
+					waitKey(30);
+				} catch (cv::Exception& e) {
+					cerr << "Error: " << e.msg << endl;
 				}
 			}
 			//cout << "Stopping camera..." << endl;
 			//camera.release();
 		} else {
 			cout << "failed to load file /usr/local/share/OpenCV/lbpcascades/lbpcascade_frontalface.xml" << endl;
+			exit(EXIT_FAILURE);
 		}
 	} else {
 		cout << "failed to open camera." << endl;
+		exit(EXIT_FAILURE);
 	}
 
 	puts("Done.");
